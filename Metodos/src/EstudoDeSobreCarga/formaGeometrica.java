7950ad82b5a0edec8c632b42f26caa5974af1c2c
Leandro Sena Zuza <leandrosenazuza@gmail.com>
2022-09-23 00:34:12 -0300

    Iniciando Sobrecarga

---
+++ Metodos/src/EstudoDeSobreCarga/formaGeometrica.java
@@ -0,0 +1,31 @@
+package EstudoDeSobreCarga;
+
+public class formaGeometrica {
+    long baseMenor;
+    long baseMaior;
+    long altura;
+
+    public long getBaseMenor() {
+        return baseMenor;
+    }
+
+    public void setBaseMenor(long baseMenor) {
+        this.baseMenor = baseMenor;
+    }
+
+    public long getBaseMaior() {
+        return baseMaior;
+    }
+
+    public void setBaseMaior(long baseMaior) {
+        this.baseMaior = baseMaior;
+    }
+
+    public long getAltura() {
+        return altura;
+    }
+
+    public void setAltura(long altura) {
+        this.altura = altura;
+    }
+}
